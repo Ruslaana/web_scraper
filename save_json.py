72d89a687837f6a760756874afe8ccd44e328036
Ruslana Onyshchuk <lalelya7@gmail.com>
2025-04-16 18:52:53 +0200

    automated multi-source news scraping, storing each article in a separate JSON file with title-based naming. Added support for source links and featured images, ensuring structured data storage

--- save_json.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def save_to_json(data, filename="data.json"):
-    try:
-        with open(filename, "w", encoding="utf-8") as file:
-            json.dump(data, file, ensure_ascii=False, indent=4)
-        print(f"✅ Data successfully saved to {filename}")
-    except Exception as e:
-        print(f"❌ Error writing JSON: {e}")
